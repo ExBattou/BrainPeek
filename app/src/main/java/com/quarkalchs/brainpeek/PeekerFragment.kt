b10a999f745a71e0142fa15cc69778d510d3cd63
ExBattou83 <adrian.narducci@gmail.com>
2020-07-19 19:51:10 -0300

    fixed how it saves the mutable list.

--- app/src/main/java/com/quarkalchs/brainpeek/PeekerFragment.kt
+++ app/src/main/java/com/quarkalchs/brainpeek/PeekerFragment.kt
@@ -6,6 +6,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.peeker_fragment.*
 
 class PeekerFragment : Fragment() {
 
@@ -27,10 +28,32 @@ class PeekerFragment : Fragment() {
         viewModel = ViewModelProviders.of(this).get(PeekerViewModel::class.java)
     }
 
-    private fun startTextsForPlay() {
-
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        start_button.setOnClickListener {
+            playWords(true)
+        }
+        pause_button.setOnClickListener {
+            playWords(false)
+        }
 
     }
 
+    private fun startTextsForPlay() {
+        var a = "Muchas veces la realidad supera a la ficción. Otras, directamente la copia. Eso ocurrió ayer en un acto por el 9 de Julio en el municipio de El Carmen, situado a pocos kilómetros de San Salvador de Jujuy.\n" +
+                "\n" +
+                "Con motivo del aniversario de la Independencia, el intendente Rodolfo Alejandro Torres pronunció un discurso que comenzó con los clásicos agradecimientos a los funcionarios del gobierno y a los empleados de la administración pública por su esfuerzo cotidiano en el marco de la pandemia de coronavirus.\n" +
+                "\n" +
+                "Luego, aseguró que la “lucha sanitaria” encontrará unidos a todos los jujeños. Sin embargo, algunas de sus palabras llamaron la atención: especialmente su arenga a “luchar una vez más por la libertad” y “no morir sin pelear”.\n" +
+                "\n" +
+                "“Tal vez sea el destino que hoy 9 de Julio, una fecha tan especial, sea tiempo de luchar una vez más por la libertad. No entraremos rendidos y en silencio hacia la enfermedad, no nos entregaremos a la pandemia y no moriremos sin pelear. Vamos a buscar vivir, vamos a buscar existir, hoy celebramos nuestro Día de la Independencia”, dijo el intendente. Su cara estaba cubierta con un tapaboca de los colores de la bandera argentina.\n" +
+                "\n" +
+                "Desafortunadamente para él, algún memorioso o fanático de las películas de ciencia ficción reconoció la estructura de su discurso. Si bien el mandatario municipal se encargó de adaptarlo al contexto de la pandemia, plagió varias partes de la emotiva arenga previa a la batalla final contra los alienígenas en el film Día de la Independencia."
 
+        viewModel.listify(a)
+    }
+
+    fun playWords(play: Boolean) {
+
+    }
 }
--- app/src/main/java/com/quarkalchs/brainpeek/PeekerViewModel.kt
+++ app/src/main/java/com/quarkalchs/brainpeek/PeekerViewModel.kt
@@ -1,21 +1,42 @@
 package com.quarkalchs.brainpeek
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class PeekerViewModel : ViewModel() {
 
     var longText = ""
     var longTextOnAList = mutableListOf<String>()
-    var currentWord = ""
+    var currentWord = MutableLiveData<String>()
+    var totalWords = 0
+    var currentPosition = 0
+    var haveToDoYerThing = false // Esto se utiliza para que, si esta en true, ejecuta el metodo doYerThang que cambia las palabras de la lista.
+    // cuando pasa a false se corta el thread.
 
-    fun listify() {
+    fun listify(a: String) {
+        longText = a
         val words = longText.split("\\s+").toTypedArray()
         for (i in words.indices) {
-            // You may want to check for a non-word character before blindly
-            // performing a replacement
-            // It may also be necessary to adjust the character class
             words[i] = words[i].replace("[^\\w]".toRegex(), "")
         }
         longTextOnAList = words.toMutableList()
+    }
+
+    suspend fun doYerThang(delayed: Long) {
+        viewModelScope.launch {
+            currentWord.value = longTextOnAList.get(currentPosition)
+            delay(delayed)
+            currentPosition++
+        }
+    }
+
+    fun showWord() : LiveData<String> {
+        return currentWord
     }
 }
